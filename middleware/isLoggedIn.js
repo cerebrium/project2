ecaf517155e23719bedb1acbdb54a63badd58773
cerebrium <nicholas.m.shankland@gmail.com>
2020-01-11 14:47:51 -0800

    changed it to scroll

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
     if (!req.user) {
-        req.flash('error', 'You must be logged in to view that page');
+        req.flash('error', '');
         res.redirect('/auth/login');
     } else {
         next();
